413a2d17ff9cd264503c2f2a95b904bcf40ae038
coderock100 <vd.ssaathvik@gmail.com>
2024-11-02 21:54:37 +0530

    get prime numbers smaller than number

---
+++ primeSieve.py
@@ -0,0 +1,18 @@
+def Sieveofreatosthenes(num):
+    prime = [True for i in range(num+1)]
+
+    p= 2
+    while (p*p <= num):
+        if(prime[p] == True):
+            for i in range(p*p, num+1, p):
+                prime[i] = False
+        p += 1
+
+    for p in range(2, num+1):
+        if prime[p]:
+            print(p)
+
+num = int(input())
+print('following are the prime numbers smaller'),
+print('than or rqual to', num)
+Sieveofreatosthenes(num)
